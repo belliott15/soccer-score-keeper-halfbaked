6cfb7a38be3dc93b1beb8b4117b8e36440203c03
Beau Elliott <belliott1516@gmail.com>
2022-03-23 12:26:25 -0700

    completed fetch-utils creategame funciton and getgames function

--- fetch-utils.js
+++ fetch-utils.js
@@ -19,12 +19,23 @@ The games are stored in the database using this data model:
 
 export async function createGame(game){
     // create a single new game in the games table using the above object
-    
+    const response = await client
+        .from('games')
+        .insert([{
+            name1: game.name1,
+            name2: game.name2,
+            score1: game.score1,
+            score2: game.score2,
+            user_id: game.user_id
+        }]);
     return checkError(response);
 }
 
 export async function getGames() {
     // select all games from the games table
+    const response = await client
+        .from('games')
+        .select('*');
 
     return checkError(response);    
 }
